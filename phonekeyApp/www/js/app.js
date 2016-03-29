597121751f37bc4d3b312f737aadaddf3c0f5ae3
jordi <jordigilnieto@gmail.com>
2016-03-29 21:34:45 +0200

    Splash

--- phonekeyApp/www/js/app.js
+++ phonekeyApp/www/js/app.js
@@ -5,28 +5,40 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    var push = new Ionic.Push({
-      "debug": true
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      var push = new Ionic.Push({
+        "debug": true
+      });
+
+      push.register(function (token) {
+        console.log("Device token:", token.token);
+      });
+
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
     });
+  })
 
-    push.register(function(token) {
-      console.log("Device token:",token.token);
-    });
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    // splash page
+      .state('splash', {
+        url: '/',
+        templateUrl: 'templates/splash.html'
+      })
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    $urlRouterProvider.otherwise('/');
+  })
